dad8e9a4fdf53ce78682309e345df2e1090eb258
root <root@bf>
2026-05-24 19:30:12 +0800

    ceart amdin pages

--- src/controllers/admin/index.ts
+++ src/controllers/admin/index.ts
@@ -4,10 +4,21 @@ import { adminUserController } from './user';
 import { adminNodeController } from './node';
 import { adminShopController } from './shop';
 import { adminTicketController } from './ticket';
+import { adminAnnouncementController } from './announcement';
+import { adminCouponController } from './coupon';
+import { adminCodeController } from './code';
+import { adminInviteController } from './invite';
+import { adminAuditController } from './audit';
 
 export const adminController = new Elysia({ prefix: '/api/v1' })
   .use(adminDashboardController)
   .use(adminUserController)
   .use(adminNodeController)
   .use(adminShopController)
-  .use(adminTicketController);
+  .use(adminTicketController)
+  .use(adminAnnouncementController)
+  .use(adminCouponController)
+  .use(adminCodeController)
+  .use(adminInviteController)
+  .use(adminAuditController);
+
